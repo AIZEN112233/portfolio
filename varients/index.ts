ecee3ee610592580f1e717f6ff60c6f168153634
Aizen <acristyano22@gmail.com>
2024-01-23 22:30:41 +0100

    feat: adding react tilt varient

--- varients/index.ts
+++ varients/index.ts
@@ -2,3 +2,15 @@ export const fromTopToBottom = {
     initial: { opacity: 0, y: -50 },
     animate: { opacity: 1, y: 0 },
 };
+
+export const defaultOptions = {
+    reverse: true,
+    max: 20,
+    perspective: 1000,
+    speed: 1000,
+    transition: true,
+    axis: null,
+    reset: true,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    scale: 1,
+};
