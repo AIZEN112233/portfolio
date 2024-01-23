042a231c46928edd58b3fb73ef72813e02b85fce
Aizen <acristyano22@gmail.com>
2024-01-23 18:48:53 +0100

    ui: Header components initial

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,25 +1,27 @@
+"use client";
 import { navLinks } from "@/constants";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { fromTopToBottom } from "@/varients";
 
 const Header = () => {
     return (
-        <header className='sticky flex justify-center top-7'>
-            <nav className='flex gap-[52px] border-4 border-primary rounded-full px-[62px] py-[17px] bg-glassy-white list-none font-400'>
+        <motion.header
+            variants={fromTopToBottom}
+            initial='initial'
+            animate='animate'
+            transition={{ duration: 1.5, ease: "easeOut" }}
+            className='sticky flex justify-center top-7'
+        >
+            <nav className='flex gap-[52px] border-4 border-primary rounded-full px-[62px] py-[17px] bg-glassy-white list-none font-normal'>
                 {navLinks.map((link) => (
                     <li key={link.title}>
                         <Link href={link.path}>{link.title}</Link>
                     </li>
                 ))}
             </nav>
-        </header>
+        </motion.header>
     );
 };
 
 export default Header;
-
-// color: #FFF;
-// text-align: center;
-// font-family: "Poller One";
-// font-style: normal;
-// font-weight: 400;
-// line-height: normal;
