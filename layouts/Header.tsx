c7cc8b07bcc3f16195395c8f1e0adc21d0eb826e
Aizen <acristyano22@gmail.com>
2024-01-23 18:37:34 +0100

    chore: update color list

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,9 +1,25 @@
+import { navLinks } from "@/constants";
+import Link from "next/link";
+
 const Header = () => {
     return (
-        <header>
-            <nav>hello world</nav>
+        <header className='sticky flex justify-center top-7'>
+            <nav className='flex gap-[52px] border-4 border-primary rounded-full px-[62px] py-[17px] bg-glassy-white list-none font-400'>
+                {navLinks.map((link) => (
+                    <li key={link.title}>
+                        <Link href={link.path}>{link.title}</Link>
+                    </li>
+                ))}
+            </nav>
         </header>
     );
 };
 
 export default Header;
+
+// color: #FFF;
+// text-align: center;
+// font-family: "Poller One";
+// font-style: normal;
+// font-weight: 400;
+// line-height: normal;
