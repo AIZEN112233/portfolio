10fdd99e3589e9b011366958184401b2fa28dd07
Aizen <acristyano22@gmail.com>
2024-01-23 19:10:44 +0100

    ui: init main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Home from "@/components/Home";
+import Projects from "@/components/Projects";
+import Rate from "@/components/Rate";
+import Skills from "@/components/Skills";
+
 const page = () => {
-    return <div>page</div>;
+    return (
+        <main>
+            <section className='screen-container'>
+                <Home />
+            </section>
+
+            <section className='screen-container'>
+                <About />
+            </section>
+
+            <section className='screen-container'>
+                <Skills />
+            </section>
+
+            <section className='screen-container'>
+                <Rate />
+            </section>
+
+            <section className='screen-container'>
+                <Projects />
+            </section>
+
+            <section className='screen-container'>
+                <Contact />
+            </section>
+        </main>
+    );
 };
 
 export default page;
