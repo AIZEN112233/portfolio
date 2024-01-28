d27564120f2a0fd01b54f517e97ed770da2a9006
Aizen <acristyano22@gmail.com>
2024-01-28 18:00:22 +0100

    chore: add icons in the constants dir

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,21 @@
+import Css from "@/components/icons/Css";
+import Html from "@/components/icons/Html";
+import Javascript from "@/components/icons/Javascript";
+import Nuxt from "@/components/icons/Nuxt";
+import Pinia from "@/components/icons/Pinia";
+import React from "@/components/icons/React";
+import Redux from "@/components/icons/Redux";
+import Next from "@/components/icons/Next";
+import Unocss from "@/components/icons/Unocss";
+import Mongo from "@/components/icons/Mongo";
+import Git from "@/components/icons/Git";
+import Node from "@/components/icons/Node";
+import Scss from "@/components/icons/Scss";
+import Tailwind from "@/components/icons/Tailwind";
+import Typescript from "@/components/icons/Typescript";
+import Github from "@/components/icons/GitHub";
+import Vue from "@/components/icons/Vue";
+
 export const navLinks = [
     {
         title: "Home",
@@ -19,4 +37,10 @@ export const navLinks = [
         title: "Projects",
         path: "#projects",
     },
+];
+
+export const icons = [
+    [Next, Unocss, Github, Javascript, Node, Mongo],
+    [Nuxt, Vue, React, Tailwind, Typescript, Css],
+    [Html, Git, Redux, Scss, Pinia],
 ];
