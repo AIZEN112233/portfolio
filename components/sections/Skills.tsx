5f00db025d58c26641ea8ef9a5586e01b0c4b6d1
Aizen <acristyano22@gmail.com>
2024-01-28 18:01:51 +0100

    ui: skills section

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -1,45 +1,60 @@
 "use client";
-import { fromBottomToTop } from "@/varients";
+import { fromBottomToTop, fromleftToRight } from "@/varients";
 import { motion } from "framer-motion";
-import Nuxt from "../icons/Nuxt";
-import React from "../icons/React";
-import Vue from "../icons/Vue";
-import Pinia from "../icons/Pinia";
-import Javascript from "../icons/Javascript";
-import Typescript from "../icons/Typescript";
-import Tailwind from "../icons/Tailwind";
-import Redux from "../icons/Redux";
-import Css from "../icons/Css";
-import Html from "../icons/Html";
+import MagnifyEffect from "../MagnifyEffect";
+import { icons } from "@/constants";
 const Skills = () => {
     return (
         <div>
-            <motion.h1
-                variants={fromBottomToTop}
-                initial='initial'
-                whileInView='animate'
-                drag
-                dragConstraints={{
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                }}
-                className='hover:(text-red bg-yellow) text-center text-[40px] font-[500]'
-            >
-                Skills
-            </motion.h1>
-            <div className='mt-24 flex justify-center gap-10'>
-                <Nuxt />
-                <React />
-                <Vue />
-                <Pinia />
-                <Javascript />
-                <Typescript />
-                <Tailwind />
-                <Redux />
-                <Css />
-                <Html />
+            <MagnifyEffect className='mx-auto w-fit p-10'>
+                <motion.h1
+                    variants={fromBottomToTop}
+                    initial='initial'
+                    whileInView='animate'
+                    transition={{ duration: 2, type: "spring" }}
+                    drag
+                    dragConstraints={{
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                    }}
+                    className='text-center text-[40px] font-[500]'
+                >
+                    Skills
+                </motion.h1>
+            </MagnifyEffect>
+            <div className='mt-24 flex flex-col items-center justify-center gap-[42px]'>
+                {icons.map((i, index) => (
+                    <motion.div
+                        variants={fromleftToRight}
+                        initial='initial'
+                        whileInView='animate'
+                        transition={{
+                            duration: 2,
+                            type: "spring",
+                            delay: index / 2,
+                        }}
+                        className='flex gap-[36px]'
+                    >
+                        {i.map((Icon, index) => (
+                            <motion.div
+                                variants={fromleftToRight}
+                                initial='initial'
+                                whileInView='animate'
+                                transition={{
+                                    duration: 2,
+                                    type: "spring",
+                                    delay: index / 2,
+                                }}
+                            >
+                                <MagnifyEffect>
+                                    <Icon />
+                                </MagnifyEffect>
+                            </motion.div>
+                        ))}
+                    </motion.div>
+                ))}
             </div>
         </div>
     );
