668a4403ec0a62cb5da3722273aca580ab01e084
Aizen <acristyano22@gmail.com>
2024-01-29 21:52:42 +0100

    chore: fix header size

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
             initial='initial'
             animate='animate'
             transition={{ duration: 1.5, type: "spring" }}
-            className='absolute top-7 flex w-full justify-center max-md:top-0 max-md:justify-between max-md:p-10'
+            className='fixed top-7 z-50 flex w-full justify-center max-md:top-0 max-md:justify-between max-md:border-b max-md:border-primary max-md:px-10 max-md:py-5 max-md:backdrop-blur'
         >
             <Link
                 href='/'
@@ -24,10 +24,11 @@ const Header = () => {
                 AIZEN
             </Link>
             <nav
-                className={`flex list-none gap-[52px] rounded-full border-4 border-primary bg-glassy-white px-[62px] py-[17px] font-normal duration-500 hover:shadow-primary-shadow max-md:absolute max-md:top-0 max-md:h-screen max-md:w-screen max-md:flex-col max-md:items-center max-md:justify-center max-md:rounded-none max-md:border-0 max-md:text-[32px] ${isOpen ? " left-0 block" : " left-0 max-md:hidden"}`}
+                className={`flex list-none gap-[52px] rounded-full border-4 border-primary bg-glassy-white px-[62px] py-[17px] font-normal duration-500 hover:shadow-primary-shadow max-md:absolute max-md:top-0 max-md:h-screen max-md:w-screen max-md:flex-col max-md:items-center max-md:justify-center max-md:rounded-none max-md:border-0 max-md:text-[32px] ${isOpen ? " left-0 max-md:translate-x-0" : "max-md:translate-x-full"}`}
             >
                 {navLinks.map((link) => (
                     <li
+                        onClick={() => setIsOpen(false)}
                         key={link.title}
                         className='duration-500 hover:scale-110'
                     >
@@ -50,3 +51,4 @@ const Header = () => {
 };
 
 export default Header;
+//${isOpen ? " left-0 block" : " left-0 max-md:hidden"}`}
